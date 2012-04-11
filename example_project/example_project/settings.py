512348b444d584f413bd5e6602e819c2dde73bd9
Chris Villa <chrisvilla@me.com>
2012-04-11 23:52:47 +0100

    example_project changes

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -118,11 +118,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
     'eventtools',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'eventtools_testapp'
 )
 
 # A sample logging configuration. The only tangible logging
